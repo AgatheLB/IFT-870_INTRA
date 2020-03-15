b6eb356ed8d08c18a7a9d35be289992db2376464
AgatheLB <agathe.lebouler@gmail.com>
2020-03-14 21:33:24 -0400

    q1 add visuals + q2A add merge

--- examenintra.py
+++ examenintra.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 standard_journal_categories = ["ANTHROPOLOGY", "ASTRONOMY AND ASTROPHYSICS", "CIRCUITS", "COMPUTER SCIENCE",
                                "DENTISTRY", "DERMATOLOGY", "ECOLOGY AND EVOLUTION", "ECONOMICS", "EDUCATION", "ENERGY",
@@ -58,6 +59,31 @@ def get_df_duplicated_rows_dropped(table):
     return table.drop_duplicates()
 
 
+def plot_categories_frequency(table, header):
+    fig, ax = plt.subplots()
+    table[header].value_counts()[0:5].plot(ax=ax, kind='bar')
+    plt.title(f'Fréquence d\'apparition des catégories de l\'attribut {header}')
+    plt.show()
+
+
+def get_mean_price_per_year():
+    mean_price_per_year = {}
+    for index, p in price.iterrows():
+        year = p['date_stamp'].year
+        if year in mean_price_per_year:
+            mean_price_per_year[year] += p['price']
+        else:
+            mean_price_per_year[year] = p['price']
+
+    for year, value in mean_price_per_year.items():
+        mean_price_per_year[year] /= len(price[price['price'] != 0])
+    return {k: v for k, v in sorted(mean_price_per_year.items(), key=lambda item: item[1], reverse=True)}
+
+
+def rename_df_headers(table, dict_headers):
+    return table.rename(columns=dict_headers)
+
+
 # %%
 
 """
@@ -72,7 +98,6 @@ statistiques descriptives au besoin.
 # TODO: visualisations
 # fréquence des valeurs
 
-# TODO: quand is_hybrid == 1, alors pub_name donne 4 valeurs
 # %%
 
 print(journal.head())
@@ -121,15 +146,27 @@ print(f"Valeurs possibles pour l'attribut category de journal:\n"
 
 # %%
 """
-On remarque qu'il existe uniquement deux valeurs pour l'attribut is_hybrid (soit 1 soit 0).
 Les attributs category et url présentent un nombre conséquent de valeurs manquantes.
 L'attribut issn présente des valeurs uniques pour chacun de ses objets.
+
+On remarque qu'il existe uniquement deux valeurs pour l'attribut is_hybrid (soit 1 soit 0). 
 """
+
+# %%
+
+print(f"Valeurs possibles de pub_name quand is_hybrid vaut 1:\n"
+      f"{journal[journal['is_hybrid'] == 1]['pub_name'].unique()}")
+
+# %%
+
+plot_categories_frequency(journal, 'pub_name')
+plot_categories_frequency(journal, 'category')
 
 # %%
 """
 ### Table price
 """
+
 # %%
 
 print(price.head())
@@ -137,9 +174,10 @@ print(price.head())
 # %%
 """
 
-price: information du prix d'un journal à une date précise, en dollar US
+price: information du prix d'une publication pour le journal associé à une date précise, en dollar US
+Si celui-ci est à 0, on peut coompendre que celui-ci est gratuit
 
-date_stamp: horodatage de l'information de prix du journal, en format années-mois-jour
+date_stamp: horodatage de l'information de prix d'une publication, en format années-mois-jour
 
 journal_id: identifiant du journal
 Les valeurs semblent suivre consistantement un format du type: 4 digits - 4 digits
@@ -147,11 +185,17 @@ Les valeurs semblent suivre consistantement un format du type: 4 digits - 4 digi
 influence_id: identifiant de l'influence
 Les valeurs suivent un format 4 digits.
 
-url: indique l'adresse url du site du journal
+url: indique l'adresse url du site de l'auteur
 
 license: indique le type de license utilisé par le journal pour les différents articles utilisés.
 
+
+On convertit l'attribut date_stamp en type date.
 """
+
+# %%
+
+price['date_stamp'] = pd.to_datetime(price['date_stamp'], errors='coerce', format='%Y-%m-%d')
 
 # %%
 
@@ -169,6 +213,15 @@ print(f"Valeurs possibles pour l'attribut license de price:\n"
 """
 Les attributs influence_id, url et license présentent une majorité de valeurs manquantes.
 """
+
+# %%
+
+mean_price_per_year = get_mean_price_per_year()
+
+plt.bar(range(len(mean_price_per_year)), mean_price_per_year.values())
+plt.xticks(range(len(mean_price_per_year)), mean_price_per_year.keys())
+plt.title("Moyenne par année des prix des publications")
+plt.show()
 
 # %%
 """
@@ -226,7 +279,7 @@ On se base sur l'attribut issn qui devrait être unique pour chaque objet de la 
 
 nb = len(journal)
 journal = get_df_duplicated_rows_dropped(journal)
-print(f"Nombre d'objets dupliqués dans journal: {nb - len(journal)}")
+print(f"Nombre d'objets dupliqués éliminés dans journal: {nb - len(journal)}")
 
 check = np.logical_not(journal['issn'].duplicated().any())
 print(f"Unicité de l'attribut issn dans la table journal: {check}")
@@ -247,7 +300,9 @@ journal_id. S'ils ne le sont pas, alors ceux sont des objets dupliqués.
 check = np.logical_not(price.index.duplicated().any())
 print(f"Unicité des indexes de la table price: {check}")
 
+nb = len(price)
 price = get_df_duplicated_rows_dropped(price)
+print(f"Nombre d'objets dupliqués éliminés dans price: {nb - len(price)}")
 
 duplicated_rows = price[price[['date_stamp', 'journal_id']].duplicated(keep=False)]
 
@@ -283,3 +338,68 @@ Troisième cas: seule la valeur de license est différente, on décide de garder
 
 for i in range(3, 39, 2):
     price = price.drop(duplicated_rows.index.values[i])
+
+# %%
+"""
+### Table influence
+Etant donné que les index étaient fournis dans le fichier original et qu'on les utilise afin d'indexer nos objets, 
+on vérifie qu'il n'existe pas de duplicata.
+
+On se base sur l'attribut issn qui devrait être unique pour chaque objet de la table, on vérifie son unicité.
+"""
+
+# %%
+
+nb = len(influence)
+influence = get_df_duplicated_rows_dropped(influence)
+print(f"Nombre d'objets dupliqués éliminés dans influence: {nb - len(influence)}")
+
+check = np.logical_not(influence['issn'].duplicated().any())
+print(f"Unicité de l'attribut issn dans la table influence: {check}")
+
+# %%
+"""
+## Merge
+Afin de simplifier les opérations, on génère une seule table reprenant les informations des trois tables.
+On vérifie d'abord si les identifiants communs aux différentes tables sont présentes dans les tables à merger.
+En premier, on vérifie si les valeurs de l'attribut issn de influence sont existantes dans l'attribut du même nom 
+dans journal.
+De même, on vérifie les valeurs de l'attribut journal_id de price sont existantes dans l'attribut issn dans journal. 
+"""
+# %%
+
+check = influence['issn'].isin(journal['issn']).any()
+print(f"Pas de valeur d'issn manquante dans journal par rapport à influence : {check}")
+
+check = price['journal_id'].isin(journal['issn']).any()
+print(f"Pas de valeur d'issn manquante dans journal par rapport à price : {check}")
+
+# %%
+"""
+On applique maintenant le merge des trois tables en deux étapes. D'abord, on merge influence dans journal, puis price
+est ensuite mergé dans le résultat du premier merge.
+"""
+# %%
+
+price = rename_df_headers(price, {"journal_id": "issn", "url": "url_autor"})
+journal = rename_df_headers(journal, {"url": "url_journal"})
+
+temp = pd.merge(journal, influence, on='issn')
+check = len(temp[temp['journal_name_x'] != temp['journal_name_y']])
+print(f"Nombre d'aberrances entre les valeurs journal_name des tables journal et influence: {check}")
+temp = temp.drop(columns=['journal_name_y'])
+temp = rename_df_headers(temp, {"journal_name_x": "journal_name"})
+
+data = pd.merge(price, temp, on='issn')
+data = get_df_duplicated_rows_dropped(data)
+
+print(f"Valeurs uniques des attributs de data:\n"
+      f"{get_uniqueness_attributes(data)}")
+print(f"Ratio de valeurs vides pour les attributs de data:\n"
+      f"{get_ratio_missing_values(data)}")
+
+# %%
+"""
+## B. Y-a-t-il une corrélation entre les catégories de journaux (attribut category) et les coûts de publication 
+(attribut price)? Justifier la réponse.
+"""
